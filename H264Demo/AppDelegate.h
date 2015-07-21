73b3632b7ad4d271181de6f66430f7ef4fd8b4cc
B1403001 <B1403001@B1403001deMacBook-Air.local>
2015-07-21 13:18:43 +0800

    Initial Commit

---
+++ H264Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  H264Demo
+//
+//  Created by B1403001 on 2015/7/21.
+//  Copyright (c) 2015年 B1403001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ H264Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  H264Demo
+//
+//  Created by B1403001 on 2015/7/21.
+//  Copyright (c) 2015年 B1403001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
